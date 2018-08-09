44f5dd863e47093d2d03b7808800420b5e836e02
肖二龙 <xiaoerlong@lemonread.com>
2018-08-09 17:46:17 +0800

    版本问题

--- Example/TRKit/TRViewController.m
+++ Example/TRKit/TRViewController.m
@@ -9,9 +9,11 @@
 #import "TRViewController.h"
 #import "NSObject+TRVersion.h"
 #import "UIButton+Countdown.h"
+#import "UIScrollView+TRRefresh.h"
 
 @interface TRViewController ()
 @property (nonatomic, strong) UIButton *btn;
+@property (nonatomic, strong) UIScrollView *scrollView;
 @end
 
 @implementation TRViewController
@@ -22,12 +24,16 @@
     
     NSLog(@"%@", [self version]);
     [self.btn countdownWithTime:60 normalTitle:@"获取验证码"];
+    [self.scrollView setNormalHeaderWithTarget:self action:@selector(refresh) lastUpdateTimeKey:nil];
+    [self.scrollView setNormalFooterWithTarget:self action:@selector(loadMore)];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)refresh {
+    
+}
+
+- (void)loadMore {
+    
 }
 
 @end
--- TRKit.podspec
+++ TRKit.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'TRKit'
-  s.version          = '0.1.3'
+  s.version          = '0.1.4'
   s.summary          = 'pod组件库'
 
 # This description is used to generate tags and improve search results.
